20cffa68dc57b9fb5a34c28dcb777ce81adef22d
Sahana P Naik <sahananaik@thewholewave.com>
2026-03-18 17:34:20 +0530

    controller updated

--- backend/src/main/java/com/resume/resume_analyzer/controller/ResumeController.java
+++ backend/src/main/java/com/resume/resume_analyzer/controller/ResumeController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*"
 
 @RestController
 //This class returns API responses (JSON)
