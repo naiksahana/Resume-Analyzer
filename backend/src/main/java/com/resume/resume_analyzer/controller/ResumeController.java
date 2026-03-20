f7a0239aa3bf9b72ac8f46df675285f330e76183
Sahana P Naik <sahananaik@thewholewave.com>
2026-03-20 10:00:47 +0530

    fix structure

--- backend/src/main/java/com/resume/resume_analyzer/controller/ResumeController.java
+++ backend/src/main/java/com/resume/resume_analyzer/controller/ResumeController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "*"
+@CrossOrigin(origins = "*")
 
 @RestController
 //This class returns API responses (JSON)
